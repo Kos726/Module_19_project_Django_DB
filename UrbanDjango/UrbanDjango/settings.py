c57f16e55855b9d7dbfcd8cc4609dae7d4eb070b
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-12-16 01:17:14 +0300

    на проверку

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.core import paginator, management
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task1',
-    'django.core.paginator',
+    #'django.core.paginator',
 ]
 
 MIDDLEWARE = [
@@ -75,13 +77,39 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_db',      # Имя базы данных
+        'USER': 'postgres',     # Пользователь PostgreSQL
+        'PASSWORD': '123',      # Пароль PostgreSQL
+        'HOST': 'localhost',    # Адрес сервера базы данных
+        'PORT': '5432',         # Порт подключения
+        }
+
+    }
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql #### _psycopg2 ####  ',
+        'NAME': #### BASE_DIR / #### 'shop_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
 
 
 # Password validation
