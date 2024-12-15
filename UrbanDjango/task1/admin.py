68fa3362417ad4e20e49a6f714cf0b6320d8be13
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-12-15 22:20:25 +0300

    на проверку

--- UrbanDjango/task1/admin.py
+++ UrbanDjango/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Game, Buyer
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'description', 'age_limited')
+    list_filter = ('size', 'cost')
+    search_fields = ('title', )  # поле для поиска
+    list_per_page = 20  # Количество объекто вдля отображения на странице
+
+
+@admin.register(Buyer)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name', )  # поле для поиска
+    list_per_page = 30  # Количество объекто вдля отображения на странице
+
+    readonly_fields = ('balance',)
